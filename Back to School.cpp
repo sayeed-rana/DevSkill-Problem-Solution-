165bc6d3711d0be7b1b6723f93a282dd75c1b616
S.M. Sayeed <sayeedcse12@gmail.com>
2017-04-27 13:07:23 +0600

    Add files via upload

---
+++ Back to School.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int cx,cy,px,py;
+    int n;
+    float r,D;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>cx>>cy>>r>>px>>py;
+        D=pow((px-cx),2)+pow((py-cy),2);
+        //D=abs(sq-r);
+        if(D==r*r)
+        {
+           cout<<"Case " <<i<<": OnCircle\n";
+        }
+
+        else if(D>r*r)
+        {
+            cout<<"Case " <<i<<": Outside\n";
+        }
+        else if(D<r*r)
+            cout<<"Case " <<i<<": Inside\n";
+    }
+    return 0;
+}
